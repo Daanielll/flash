628bc9a16254685f4990c5af14ebc98365a779c1
Daanielll <45535265+Daanielll@users.noreply.github.com>
2019-11-29 22:04:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -111,7 +111,16 @@ bot.on("message", async message => {
           }
           if(cmd === `${prefix}addrole`){
             if(!message.member.hasPermission(["ADMINISTRATOR"])) return message.reply(" אתה לא יכול להשתמש בפקודה הזאת ")
-
+              
+            let grembed = new Discord.RichEmbed()
+            .setColor(colours.red)
+            .setTitle(`**${rMember.user.username} הוספת רול ל**`)
+            .setThumbnail(rMember.user.displayAvatarURL)
+            .addField("Role Added By: ", message.member.username)
+            .addField("Role Added:", role.name)
+            .setFooter(`Date: ${message.createdAt.toLocaleString()}`, bot.user.displayAvatarURL)
+            let sChannel = message.guild.channels.find(c => c.name === "（🔷）לוג-רולים")
+            
             let rMember = message.mentions.members.first() || message.guild.members.find(m => m.user.tag === args[0]) || message.guild.members.get(args[0])
             if(!rMember) return message.reply(" לא בחרת משתמש להביא לו את הרול.")
             let role = message.guild.roles.find(r => r.name == args[1]) || message.guild.roles.find(r => r.id == args[1]) || message.mentions.roles.first()
@@ -125,14 +134,7 @@ bot.on("message", async message => {
                 await rMember.addRole(role.id).catch(e => console.log(e.message))
                 message.reply(`.${role.name} קיבל את הרול ${rMember.displayName} המשתמש`)
             
-              let grembed = new Discord.Richembed()
-            .setColor(colours.red)
-            .setTitle(`**${rMember.user.username} הוספת רול ל**`)
-            .setThumbnail(rMember.user.displayAvatarURL)
-            .addField("Role Added By: ", message.member.username)
-            .addField("Role Added:", role.name)
-            .setFooter(`Date: ${message.createdAt.toLocaleString()}`, bot.user.displayAvatarURL)
-            let sChannel = message.guild.channels.find(c => c.name === "（🔷）לוג-רולים")
+              
             sChannel.send({embed: grembed})
             }
          }
@@ -140,7 +142,16 @@ bot.on("message", async message => {
             
          if(cmd === `${prefix}removerole`){
             if(!message.member.hasPermission(["ADMINISTRATOR"])) return message.reply(" אתה לא יכול להשתמש בפקודה הזאת ")
-
+            
+             let rrembed = new Discord.RichEmbed()
+            .setColor(colours.red)
+            .setTitle(`**${rMember.user.username} הורדת רול ל**`)
+            .setThumbnail(rMember.user.displayAvatarURL)
+            .addField("Role Removed By: ", message.member.username)
+            .addField("Role Removed:", role.name)
+            .setFooter(`Date: ${message.createdAt.toLocaleString()}`, bot.user.displayAvatarURL)
+            let sChannel = message.guild.channels.find(c => c.name === "（🔷）לוג-רולים")
+            
             let rMember = message.mentions.members.first() || message.guild.members.find(m => m.user.tag === args[0]) || message.guild.members.get(args[0])
             if(!rMember) return message.reply(" לא בחרת משתמש להוריד לו את הרול.")
             let role = message.guild.roles.find(r => r.name == args[1]) || message.guild.roles.find(r => r.id == args[1]) || message.mentions.roles.first()
@@ -153,14 +164,7 @@ bot.on("message", async message => {
             } else {
                 await rMember.removeRole(role.id).catch(e => console.log(e.message))
                 message.reply(`.${role.name} ירד הרול ${rMember.displayName} למשתמש`)
-                let rrembed = new Discord.Richembed()
-            .setColor(colours.red)
-            .setTitle(`**${rMember.user.username} הורדת רול ל**`)
-            .setThumbnail(rMember.user.displayAvatarURL)
-            .addField("Role Removed By: ", message.member.username)
-            .addField("Role Removed:", role.name)
-            .setFooter(`Date: ${message.createdAt.toLocaleString()}`, bot.user.displayAvatarURL)
-            let sChannel = message.guild.channels.find(c => c.name === "（🔷）לוג-רולים")
+                
               sChannel.send(rrembed)
              message.channel.send({embed: rrembed});
          }
