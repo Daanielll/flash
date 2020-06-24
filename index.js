3855a29bec501632fea9e91fa692a5a587cc12d6
Daanielll <45535265+Daanielll@users.noreply.github.com>
2020-06-25 00:07:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,191 +1,44 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const colours = require("./colours.json");
+const { Client, Collection } = require("discord.js");
+const { RichEmbed } = require("discord.js")
 const bot = new Discord.Client({disableEveryOne: true});
 let srole = "<@&619515952928784393>"
 let cooldown = new Set();
-const token = process.env.BOT_TOKEN;
 let cdseconds = 60;
 
 bot.on("ready", () => {
     console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} Servers.`);
     let myGuild = bot.guilds.get('551449197904265216')
     let mCount = myGuild.memberCount;
-  bot.user.setActivity(`${mCount} Members In FlashGG's Server`, {type: 'WATCHING'});     
+  bot.user.setActivity(`אני שונא את ליעד`, {type: 'WATCHING'});     
    
   });
 bot.on('guildMemberAdd', member => {
-      let myGuild = bot.guilds.get('551449197904265216')
-    let mCount = myGuild.memberCount;
-   bot.user.setActivity(`${mCount} Members In FlashGG's Server`, {type: 'WATCHING'});     
+      let avoided = member.guild.members.find(m => m.id === "176276100404477954");
+  
+  let myGuild = bot.guilds.get('719651237422825472')
+    const welcomeChannel = member.guild.channels.find(channel => channel.id === "719651777481539634");
+   if(!welcomeChannel) return; 
+  
+    if(member == avoided)
+    {     
+      avoided.kick();
+      let embed = new RichEmbed()
+    .setThumbnail(member.user.displayAvatarURL)
+    .setTitle(`${member.user.username}  ברוכים הבאים לשרת!`)
+   .setDescription(`לצערנו, אתה לא רצוי בשרת!`)
+   .addField('בהצלחה בהמשך!')
+
+   welcomeChannel.send(embed)
+   avoided.send(embed)
    
   });
 
-bot.on('guildMemberRemove', member => {
-      let myGuild = bot.guilds.get('551449197904265216')
-    let mCount = myGuild.memberCount;
-   bot.user.setActivity(`${mCount} Members in FlashGG's server`, {type: 'WATCHING'});     
-  
-  });
-
-bot.on("message", async message => {
-    if(message.author.bot || message.channel.type === "dm") return;
-
-    let prefix = botconfig.prefix;
-    let messageArray = message.content.split(" ")
-    let cmd = messageArray[0].toLowerCase();
-    let args = messageArray.slice(1);
-
-
-    if(cmd === `${prefix}help`){
-        let sEmbed = new Discord.RichEmbed()
-        .setColor(colours.red)
-        .setTitle("📃__**פקודות**__📃")
-        .setThumbnail(message.guild.iconURL)
-        .addField("** **", "**!yt** - שולח קישור לערוץ יוטיוב של פלאש")
-        .addField("** **", "**!membercount** - מראה כמה אנשים יש בשרת", true)
-        .addField("** **", "**!invite** - שולח קישור לשרת הדיסקורד של פלאש")
-        .addField("** **", "**!helpme** - קורא לצוות לעזרה")
-        .addField("** **", "**!form** - שולח את הטפסים להצטרפות לצוות", true)
-        .setFooter("FleshGG Bot", bot.user.displayAvatarURL)
-        message.channel.send({embed: sEmbed});
-    }
-    if(cmd === `${prefix}invite`){
-        let dEmbed = new Discord.RichEmbed()
-        .setColor(colours.red)
-        .setTitle("** קישור לשרת הדיסקורד: **")
-        .addField("Discord" , "https://discord.gg/6Fvbxwc")
-        message.channel.send({embed: dEmbed});
-    }
-    if(cmd === `${prefix}yt`){
-        let yEmbed = new Discord.RichEmbed()
-        .setColor(colours.red)
-        .setTitle("** קישור לערוץ היוטיוב: **")
-        .addField("YouTube" , "https://www.youtube.com/channel/UCvVq3ZaBOesSWpCkI048_6Q")
-        message.channel.send({embed: yEmbed});
-    }
-    let mEmbed = new Discord.RichEmbed()
-        .setColor(colours.red)
-        .setTitle(message.author.username + `** היי**` )
-        .addField("** **" , "**הפקודות שלי מתחילות ב - !**")
-        .addField("** **", "**אם אתה צריך עזרה מהצוות תכתוב !helpme**")
-
-        if (message.mentions.users.first() === bot.user){
         
-        message.channel.send(`Hey **${message.author.username}**, my prefix is **"!"** \nIf you need help type ` + "`!help`")
-        }
-    
-    if(cmd === `${prefix}form`){
-        let fEmbed = new Discord.RichEmbed()
-        .setColor(colours.red)
-        .setTitle("** טופס הצטרפות לצוות **")
-        .addField("Google Forms" , "https://forms.gle/qRZQxoCHKJ5KaJ7b6")
-        message.channel.send({embed: fEmbed});
-    }
-
-    if(cmd === `${prefix}helpme`){
-        let reason = args.join(' ');
-        if(!reason) reason = "None";
-        if(cooldown.has(message.author.id)){
-            return message.reply(" אתה צריך לחכות דקה אחרי בקשת עזרה ")
-        } else{
-            message.channel.send(`**${srole}, ${message.author.username}** needs your help! \nReason: **${reason}**`)
-            cooldown.add(message.author.id);
-        }
-        setTimeout(() =>{
-            cooldown.delete(message.author.id)
-        }, cdseconds * 1000)
-    }
-    
-    if(cmd === `${prefix}membercount`){
-        let myGuild = bot.guilds.get('551449197904265216')
-    let mCount = myGuild.memberCount;
-    message.channel.send(`!יש **${mCount}** אנשים בשרת`)
-    
-    }
-    
-    if(cmd === `${prefix}say`)
-            {
-        if(!message.member.hasPermission(["ADMINISTRATOR"])) return message.reply("** אתה לא יכול להשתמש בפעולה הזאת **")
-
-        let argsresault;
-        let mChannel = message.mentions.channels.first()
-  
-          message.delete()
-          if(mChannel)
-          {
-              argsresault = args.slice(1).join(" ")
-              mChannel.send(argsresault)
-          }
-          else
-          {
-              argsresault = args.join(" ")
-              message.channel.send(argsresault)
-          }
-          }
-          if(cmd === `${prefix}addrole`){
-            if(!message.member.hasPermission(["ADMINISTRATOR"])) return message.reply(" אתה לא יכול להשתמש בפקודה הזאת ")
-              
-           
-            
-            let rMember = message.mentions.members.first() || message.guild.members.find(m => m.user.tag === args[0]) || message.guild.members.get(args[0])
-            if(!rMember) return message.reply(" לא בחרת משתמש להביא לו את הרול.")
-            let role = message.guild.roles.find(r => r.name == args[1]) || message.guild.roles.find(r => r.id == args[1]) || message.mentions.roles.first()
-            if (!role) return message.reply(" לא כתבת רול להביא למשתמש.")
-
-           if(!message.guild.me.hasPermission(["ADMINISTRATOR"])) return message.reply(" אין לי גישה לעשות את זה.") 
-
-            if(rMember.roles.has(role.id)) {
-                return message.reply(`.${role.name} כבר יש את הרול ${rMember.displayName} למשתמש`)
-            } else {
-                await rMember.addRole(role.id).catch(e => console.log(e.message))
-                message.reply(`.${role.name} קיבל את הרול ${rMember.displayName} המשתמש`)
-                 let grembed = new Discord.RichEmbed()
-            .setColor(colours.red)
-            .setTitle(`**${rMember.user.username} הוספת רול ל**`)
-            .setThumbnail(rMember.user.displayAvatarURL)
-             .addField("Member", `${rMember}`)
-            .addField("Role Added By: ", message.author.username)
-            .addField("Role Added:", role.name)
-            .setFooter(`Date: ${message.createdAt.toLocaleString()}`, bot.user.displayAvatarURL)
-            let sChannel = message.guild.channels.find(c => c.name === "（🔷）לוג-רולים")
-              
-            sChannel.send({embed: grembed})
-            }
-         }
-        
-            
-         if(cmd === `${prefix}removerole`){
-            if(!message.member.hasPermission(["ADMINISTRATOR"])) return message.reply(" אתה לא יכול להשתמש בפקודה הזאת ")
-            
-             
-            
-            let rMember = message.mentions.members.first() || message.guild.members.find(m => m.user.tag === args[0]) || message.guild.members.get(args[0])
-            if(!rMember) return message.reply(" לא בחרת משתמש להוריד לו את הרול.")
-            let role = message.guild.roles.find(r => r.name == args[1]) || message.guild.roles.find(r => r.id == args[1]) || message.mentions.roles.first()
-            if (!role) return message.reply(" לא כתבת רול להוריד למשתמש.")
-
-           if(!message.guild.me.hasPermission(["ADMINISTRATOR"])) return message.reply(" אין לי גישה לעשות את זה.") 
-
-            if(!rMember.roles.has(role.id)) {
-                return message.reply(`.${role.name} אין את הרול ${rMember.displayName} למשתמש`)
-            } else {
-                await rMember.removeRole(role.id).catch(e => console.log(e.message))
-                message.reply(`.${role.name} ירד הרול ${rMember.displayName} למשתמש`)
-                let rrembed = new Discord.RichEmbed()
-            .setColor(colours.red)
-            .setTitle(`**${rMember.user.username} הורדת רול ל**`)
-            .setThumbnail(rMember.user.displayAvatarURL)
-            .addField("Member", `${rMember}`)
-            .addField("Role Removed By: ", message.author.username)
-            .addField("Role Removed:", role.name)
-            .setFooter(`Date: ${message.createdAt.toLocaleString()}`, bot.user.displayAvatarURL)
-            let sChannel = message.guild.channels.find(c => c.name === "（🔷）לוג-רולים")
-            sChannel.send({embed: rrembed})
-         }
-         }
 })
 
-bot.login(token);
+bot.login('NzI1NDQzMTU0NDUwMzE3MzQ0.XvO53w.8FHGFTka888Q0EAaAR3nzufKzB4');
 
 
